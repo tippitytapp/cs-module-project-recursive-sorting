e1cd429cabb583c4bd2828e34e25813ad9beb285
Marc Tapp <marc.tapp@gmail.com>
2020-06-16 17:34:30 -0400

    finished MVP

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -11,7 +11,42 @@ def merge(arrA, arrB):
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
+    # if there is an array with more than 1 element
+    if len(arr) > 1:
+        # calculate the mid
+        mid = len(arr) //2
+        # set new array to all elements from the 0-mid
+        newarr1 = arr[:mid]
+        # set new array to all elements from the mid - end
+        newarr2 = arr[mid:]
+        # preform recursion on both new arrays
+        merge_sort(newarr1)
+        merge_sort(newarr2)
+        # set variable for new array 1
+        na1 = 0
+        # set variable for new array 2
+        na2 = 0
+        # set variable for the original array
+        oarr = 0
+        while na1 < len(newarr1) and na2 < len(newarr2):
+            if newarr1[na1] < newarr2[na2]:
+                arr[oarr] = newarr1[na1]
+                na1 = na1 + 1
+            else:
+                arr[oarr] = newarr2[na2]
+                na2 = na2 + 1
+            oarr = oarr + 1
 
+        while na1 < len(newarr1):
+            arr[oarr] = newarr1[na1]
+            na1 = na1 + 1
+            oarr = oarr + 1
+        
+        while na2 < len(newarr2):
+            arr[oarr] = newarr2[na2]
+            na2 = na2 + 1
+            oarr = oarr + 1
+    return arr
 
     return arr
 
