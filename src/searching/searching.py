03289aa6d8234da8c70d283f24f59dab4774052c
Marc Tapp <marc.tapp@gmail.com>
2020-06-16 11:19:32 -0400

    started woring on he binary_search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,26 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    if end >= start: 
+  
+        mid = (end + start) // 2
+  
+        # If element is present at the middle itself 
+        if arr[mid] == target: 
+            return mid 
+  
+        # If element is smaller than mid, then it can only 
+        # be present in left subarray 
+        elif arr[mid] > target: 
+            return binary_search(arr, start, mid - 1, target) 
+  
+        # Else the element can only be present in right subarray 
+        else: 
+            return binary_search(arr, mid + 1, end, target) 
+  
+    else: 
+        # Element is not present in the array 
+        return -1
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -11,4 +31,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
